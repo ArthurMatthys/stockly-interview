d22e9316f879e1301fb64b51037313d3b9987ea0
Arthur Matthys <arthur@matthys.fr>
2022-02-21 16:03:50 +0100

    First solution working

---
+++ resolve.py
@@ -0,0 +1,47 @@
+import math
+
+
+def read_file(filename):
+    with open(filename, "r") as f:
+        content = f.readlines()
+    adj = [lines.replace("\n", "").split(",") for lines in content]
+    max_weigth = 0
+    for i, line in enumerate(adj):
+        for j, val in enumerate(line):
+            if val == "-":
+                adj[i][j] = math.inf
+            else:
+                adj[i][j] = int(val)
+                max_weigth += int(val)
+    return adj, max_weigth
+
+
+def find_min(adj, lst_opened):
+    min_paths = math.inf
+    new_node = 0
+    for opened in lst_opened:
+        min_path = min(adj[opened])
+        node_index = adj[opened].index(min_path)
+        if min_path < min_paths:
+            min_paths = min_path
+            new_node = node_index
+    return new_node, min_paths
+
+
+def main(init):
+    adj, max_weigth = read_file("p107_network.txt")
+    lst_opened = [init]
+    tot_weigth = 0
+    while len(lst_opened) != len(adj):
+        new_node, weigth = find_min(adj, lst_opened)
+        tot_weigth += weigth
+        for opened in lst_opened:
+            adj[opened][new_node] = math.inf
+            adj[new_node][opened] = math.inf
+        lst_opened.append(new_node)
+    print(max_weigth / 2 - tot_weigth)
+
+
+if __name__ == "__main__":
+    for i in range(40):
+        main(i)
